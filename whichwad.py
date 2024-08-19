7af260f3089d0003521d21e173cbeccc887ce0f6
Erty <erty.gamedev@gmail.com>
2024-08-19 12:48:11 +0200

    Show help when no args are used, version command

--- whichwad.py
+++ whichwad.py
@@ -1,10 +1,12 @@
-from typing import List, Final, Union, Literal
+from typing import List, Final, Union, Literal, Optional
 from pathlib import Path
 import typer
 from typing_extensions import Annotated
 from rich.console import Console
 from rich.theme import Theme
 from wad3_reader import Wad3Reader
+
+__version__ = '1.0.0'
 
 whichwad_theme = Theme({
     'success': 'bold green',
@@ -27,6 +29,11 @@ WAD_SKIP_LIST: Final[List[str]] = [
 
 app = typer.Typer()
 
+
+def show_version(show: bool) -> None:
+    if show:
+        console.print(f"Which WAD v{__version__}", style='info', highlight=False)
+        raise typer.Exit()
 
 def unsteampipe(modpath: Path) -> Path:
     for pipe in STEAM_PIPES:
@@ -58,27 +65,34 @@ def find_texture_in_wad(modpath: Path, texture: str) -> Union[Path, Literal[Fals
     return False
 
 
-@app.command()
+@app.command(no_args_is_help=True)
 def main(
-        mod_path: Annotated[str, typer.Argument(
+        mod_path: Annotated[Path, typer.Argument(
             help='path to the mod with the WAD files e.g. ".../steamapps/Half-Life/valve"',
             show_default=False)],
         texture: Annotated[str, typer.Argument(
             help='texture(s) to search for, use ";" to delimit multiple textures',
             show_default=False)],
+        version: Annotated[Optional[bool], typer.Option(
+            '--version', '-v', callback=show_version, is_eager=True,
+            help='print application version', rich_help_panel=None)] = None,
         ) -> None:
+    """Find which WAD in the mod path contains the specified texture"""
 
-    modpath = unsteampipe(Path(mod_path))
+    if not mod_path.is_dir():
+        console.print(f"{mod_path} is not a directory", style='error')
+        raise typer.Exit(1)
 
+    mod_path = unsteampipe(mod_path)
     textures = texture.split(';')
 
     for tex in textures:
-        found_wad = find_texture_in_wad(modpath, tex)
+        found_wad = find_texture_in_wad(mod_path, tex)
         tex = tex.upper()
 
         if not found_wad:
             console.print(
-                f"Texture {tex} not found in any WAD in [not bold]{modpath}[/not bold]",
+                f"Texture {tex} not found in any WAD in [not bold]{mod_path}[/not bold]",
                 style='error')
             continue
 
